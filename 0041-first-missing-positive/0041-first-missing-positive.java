07a6d59eb94914dbde3dabd783e56b0d5da0edfe
Risalattt <risalatriyaz3@gmail.com>
2026-03-09 19:55:59 +0530

    Time: 1 ms (99.77%), Space: 71.4 MB (46.61%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -3,18 +3,20 @@ class Solution {
         int n = nums.length;
         for(int i = 0; i < n; i++){
             int element = nums[i];
-            while(nums[i] >= 1 && nums[i] <= n && nums[element -1] != nums[i]){
-              element = nums[i];
-              int temp = nums[element -1];
-              nums[element -1] = nums[i];
-              nums[i] = temp;
+            while(nums[i] <= n && nums[i] > 0 && nums[element -1] != nums[i]){
+                     int temp = nums[element - 1];
+                     nums[element - 1] = nums[i];
+                     nums[i] = temp;
+                     element = nums[i];
+
             }
         }
-        for(int i = 0; i<n; i++){
+        for(int i = 0; i < n ; i++){
             if(nums[i] != i + 1){
                 return i + 1;
             }
         }
         return n + 1;
+        
     }
 }
