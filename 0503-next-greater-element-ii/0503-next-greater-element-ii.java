343e14672621556ac9ea8b5329d878b4f7c8320a
Risalattt <risalatriyaz3@gmail.com>
2026-03-14 13:08:03 +0530

    Time: 11 ms (65.33%), Space: 48.9 MB (10.15%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -2,24 +2,24 @@ class Solution {
     public int[] nextGreaterElements(int[] nums) {
         Stack<Integer> stack = new Stack<>();
         stack.push(0);
-        int[] answer = new int[nums.length];
-        for(int i = 1; i < nums.length; i++){
+        int[] ans = new int[nums.length];
+        for(int i =1; i < nums.length; i++){
             while(stack.size() > 0 && nums[stack.peek()] < nums[i]){
-                answer[stack.peek()] = nums[i];
+                ans[stack.peek()] = nums[i];
                 stack.pop();
-            };
+            }
             stack.push(i);
         }
-        for(int i = 0 ; i< nums.length; i++){
+        for(int i = 0; i< nums.length; i++){
             while(stack.size() > 0 && nums[stack.peek()] < nums[i]){
-                answer[stack.peek()] = nums[i];
+                ans[stack.peek()] = nums[i];
                 stack.pop();
             }
         }
         while(stack.size() > 0){
-            answer[stack.peek()] = -1;
+            ans[stack.peek()] = -1;
             stack.pop();
         }
-        return answer;
-    }
+        return ans;
+   }
 }
